d17116100a5f72c97cf0c8031e551f0d4175bfda
vitoreribeiro <vitor.e.ribeirosilva@gmail.com>
2025-03-10 17:19:13 -0300

    Obtendo um usuario por id

--- src/main/java/com/vitordev/workshopmongo/resources/UserResource.java
+++ src/main/java/com/vitordev/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,4 +28,9 @@ public class UserResource {
 		return ResponseEntity.ok().body(listDto);
 	}
 
+	@GetMapping("/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		UserDTO userDto = new UserDTO (service.findById(id));
+		return  ResponseEntity.ok().body(userDto);
+	}
 }
