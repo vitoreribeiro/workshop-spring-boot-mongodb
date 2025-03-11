de55e17275381bc5fddf7c8439a59d34064ad0f0
vitoreribeiro <vitor.e.ribeirosilva@gmail.com>
2025-03-11 20:22:09 -0300

    Delecao de usuario com DELETE

--- src/main/java/com/vitordev/workshopmongo/resources/UserResource.java
+++ src/main/java/com/vitordev/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,11 +39,17 @@ public class UserResource {
 		return  ResponseEntity.ok().body(userDto);
 	}
 	
-	@PostMapping()
+	@PostMapping
 	public ResponseEntity<Void> insert(@RequestBody UserDTO dto) {
 		User user = service.fromDTO(dto);
 		service.insert(user);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/vitordev/workshopmongo/services/UserService.java
+++ src/main/java/com/vitordev/workshopmongo/services/UserService.java
@@ -33,4 +33,9 @@ public class UserService {
 	public User fromDTO(UserDTO dto) {	
 		return new User(dto.getId(), dto.getName(), dto.getEmail());
 	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
 }
